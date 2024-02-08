0030f30f1e48e40bd667bede23668c43657fac3b
Jacob Christ <jacob@pontech.com>
2024-02-07 19:32:38 -0800

    Renamed simpserial.lua to serialbreak.lua

--- sd/serialbreak.lua
+++ sd/serialbreak.lua
@@ -1,18 +1,27 @@
 ----------------------------------------------------------------------
--- ezLCD Serial Communications simple application example
+-- ezLCD Serial Break Example
+--
+-- This example demonstrates how to use the serial port to stop the
+-- Lua script.  The script will run until a "b" is received on the
+-- serial port.  The script will then stop.
 --
 -- Created  01/27/2024  -  Jacob Christ
 ----------------------------------------------------------------------
 
+-- Set debug_port to one of thsee Serial Port Numbers:
 -- 0=Main UART (default)
 -- 1=Second UART
 -- 2=USB Virtual Comm Port
 -- 3=ESP32 UART0
 -- 4=Third UART (SE-2023 RS232)
+debug_port = 2 -- USB Virtual Comm Port
 
-debug_port = 2
-stop_lua = 0
+
+Stop_lua = 0 -- flag to stop the Lua script when set to 1
+
+-----------------------
 -- Event Handelers
+-----------------------
 -- Serial Port Event
 function DebugPortReceiveFunction(byte)
 	ez.SerialTx(byte, debug_port, 1)
@@ -21,6 +30,10 @@ function DebugPortReceiveFunction(byte)
 		stop_lua = 1
 	end
 end
+
+-----------------------
+-- Main
+-----------------------
 
 ez.Cls(ez.RGB(255,0,255))
 
@@ -31,9 +44,9 @@ ez.SerialOpen("DebugPortReceiveFunction", debug_port)
 while 1 do
 	-- Wait 10 seconds for USB to enumerate
 	ez.Wait_ms(2500)
-	ez.Cls(ez.RGB(255,0,0))
+	ez.Cls(ez.RGB(255,0,0)) -- Change the screen color to red
 	ez.Wait_ms(2500)
-	ez.Cls(ez.RGB(0,255,0))
+	ez.Cls(ez.RGB(0,255,0)) -- Change the screen color to green
 	ez.SerialTx("**********************************************************************\r\n", debug_port, 80)
 	ez.SerialTx("* EarthLCD Serial Communications Scale\r\n", debug_port, 80)
 	ez.SerialTx("**********************************************************************\r\n", debug_port, 80)
@@ -44,8 +57,10 @@ while 1 do
 	if stop_lua == 1 then
 		ez.SerialTx("Lua Stopped\r\n", debug_port, 80)
 		ez.Cls(ez.RGB(0,0,0))
-		ez.SerialClose(debug_port)
 		break
 	end
 end
+
+ez.SerialTx("Exited main loop\r\n", debug_port, 80)
+ez.SerialClose(debug_port)
 
